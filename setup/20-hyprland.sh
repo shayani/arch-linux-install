80f699799d10ec09f3495b1fff4ebb272284d3d0
Fernando Shayani <shayani@gmail.com>
2026-06-27 20:02:58 -0300

    add: hypridle, hyprpaper, xdg-desktop-portal-hyprland no setup

--- setup/20-hyprland.sh
+++ setup/20-hyprland.sh
@@ -5,6 +5,7 @@ DIR=$(cd "$(dirname "$0")" && pwd); source "$DIR/lib.sh"
 
 header "Hyprland + Wayland"
 pacotes hyprland hyprlauncher waybar rofi-wayland alacritty
+pacotes hypridle hyprpaper
 pacotes dunst libnotify
 pacotes polkit-kde-agent xdg-desktop-portal-hyprland
 pacotes hyprpolkitagent
