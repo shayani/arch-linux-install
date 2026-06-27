052989de946c6d56f99bf3645b0dedb3accb18ab
Fernando Shayani <shayani@gmail.com>
2026-06-27 19:57:52 -0300

    add: hyprpolkitagent no 20-hyprland (authentication agent)

--- setup/20-hyprland.sh
+++ setup/20-hyprland.sh
@@ -7,5 +7,6 @@ header "Hyprland + Wayland"
 pacotes hyprland hyprlauncher waybar rofi-wayland alacritty
 pacotes dunst libnotify
 pacotes polkit-kde-agent xdg-desktop-portal-hyprland
+pacotes hyprpolkitagent
 pacotes qt5-wayland qt6-wayland
 pacotes thunar thunar-archive-plugin file-roller
